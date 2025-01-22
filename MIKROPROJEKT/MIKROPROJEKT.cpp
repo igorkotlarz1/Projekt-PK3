05b71a0c3916f64b64b8d1bdfdbee86fd3099ad2
igorkotlarz1 <igorkotlarz@gmail.com>
2025-01-23 00:30:21 +0100

    23.01.25 - Usuwanie wezla z drzewa

--- MIKROPROJEKT/MIKROPROJEKT.cpp
+++ MIKROPROJEKT/MIKROPROJEKT.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include <exception>
 #include <functional>
-//#include <conio.h> 
+#include <conio.h> 
 
 class Movie
 {
@@ -18,6 +18,7 @@ public:
     Movie(std::string title, std::string director, int release_year, double rating, std::string genre) 
         : title(title),director(director),release_year(release_year),rating(rating),genre(genre){}
     Movie(std::string title, std::string director) : title(title), director(director){}
+    Movie(std::string title): title(title){}
 
     bool operator <(const Movie& other_movie)
     {
@@ -38,8 +39,8 @@ public:
     std::string getTitleUpper() const 
     { 
         std::string temp = title;
-        //for (size_t i = 0; i < title.size(); i++)        
-          //  temp[i] = std::toupper(title[i]);       
+        for (size_t i = 0; i < title.size(); i++)        
+           temp[i] = std::toupper(title[i]);       
         return temp;
     }
 
@@ -79,7 +80,7 @@ class BinaryTree
 private:
     std::unique_ptr<Node<T>> root;
 
-    void add_node(std::unique_ptr<Node<T>>& current, T value)
+    void add_node(std::unique_ptr<Node<T>>& current, const T& value)
     {
         if (!current)
             current = std::make_unique<Node<T>>(std::move(value));
@@ -90,38 +91,37 @@ private:
         else
         {}
     }
-    Node<T>* delete_node(Node<T>*current, const T& value)
+    Node<T>* delete_node(std::unique_ptr<Node<T>>& current, T& value)
     {
-        if(current == nullptr)
-        return current;
+        if(!current)
+            return current;
         
         if(value< current->data)
-            current->left = delete_node(current->left.get(),value);    
+            delete_node(current->left,value);    
         else if(value>current->data)
-            current->right = delete_node(current->right.get(),value);
-
-        // zmatchowalismy value z wezlem, tera dokonujemy operacji usuniecia
-        // jezeli lewe dziecko to null ptr, to ustawiamy nasza wartosc na wartosc prawego dziecka
-
-        if(current->left == nullptr)
-        {
-            Node<T>* temp = current->right.get();
-            current = nullptr;
-            return temp;
-        }
-        else if(current->right == nullptr)
-        {
-            Node<T>* temp = current->left.get();
-            current = nullptr;
-            return temp;
-        }
+            delete_node(current->right,value);
         else
         {
-        
+            if (!current->left) //wezel nie ma lewego dziecka
+                return current.release();//std::move(current->right);
+            else if (!current->right) //wezel nie ma prawego dziecka
+                return current.release();//std::move(current->left);
+            else //wezel ma oby dwoje dzieci
+            {
+                //Szukamy najmniejszego potomka w prawym poddrzewie metoda pomocnicza
+                Node<T>* successor = find_min(current->right);
+                current->data = successor->data;
+
+                delete_node(current->right, successor->data);
+            }
         }
-
-
-
+       return current.get();     
+    }
+    Node<T>* find_min(const std::unique_ptr<Node<T>>& current) const
+    {
+        if (!current || !current->left)
+            return current.get();
+        return find_min(current->left);
     }
 
     Node<T>* find_node(Node<T>* current, const T& value) const
@@ -164,6 +164,10 @@ public:
     {
         add_node(root, std::move(value));
     }
+    void delete_(T& value)
+    {
+        delete_node(root, value);
+    }
 
     Node<T>* find(const T& value) const
     {
@@ -192,150 +196,7 @@ public:
         return *this;
     }
 };
-/*
-template<typename T>
-class Element
-{
-public:
-    T data;
-    std::shared_ptr<Element<T>> next;
 
-    Element(T data) : data(data), next(nullptr){}
-};
-template<typename T>
-class List
-{
-private:
-   std::shared_ptr<Element<T>> head, tail;
-   
-public:
-    size_t size;
-    List() : head(nullptr),tail(nullptr),size(0) {}
-    List(const List<T>& other_list) : head(nullptr), tail(nullptr), size(0) 
-    {
-        auto current_element = other_list.head;
-        while (current_element)
-        {
-            push_back(current_element->data);
-            current_element = current_element->next;
-        }
-    }
-    //List(const &List)
-    
-    void push_front(const T& data)
-    {
-        auto new_element = std::make_shared<Element<T>>(data);
-
-        if (head) //if(head!=nullptr)       
-            new_element->next = head;       
-        else
-            tail = new_element;
-
-        head = new_element;
-        size++;
-    }
-    void push_back(const T& data)
-    {
-        auto new_element = std::make_shared<Element<T>>(data);
-
-        if (!head) //if(head==nullptr)        
-           head = tail = new_element;
-        else
-        {
-            tail->next = new_element;  
-            tail = new_element;
-        }
-        size++;
-    }
-    void print()
-    {
-        if (!head)
-        {
-            std::cout << "List is empty" << std::endl;
-            return;
-        }
-        auto current_element = head;
-
-        //std::cout << "[ ";
-        while (current_element)
-        {
-            std::cout << current_element->data << " ";
-            current_element = current_element->next;
-        }
-        //std::cout << "]"<<std::endl;
-    }     
-    
-    List<T>& operator =(List<T>& other_list)
-    {
-        if (this != other_list)
-        {
-            clear();
-            auto current_element = other_list.head;
-            while (current_element)//!= nullptr
-            {
-                push_back(current_element->data);
-                current_element = current_element->next;
-            }
-        }
-        return *this;
-    }
-    
-
-    class Iterator
-    {
-    private:
-        std::shared_ptr<Element<T>> current;
-    public:
-        Iterator(std::shared_ptr<Element<T>> start):current(start){}
-
-        T& operator*() { return current->data; }
-        T* operator->() { return &(current->data); }
-        Iterator& operator++()
-        {
-            if (current) //current != nullptr
-                current = current->next;
-            return *this;
-        }
-        bool operator ==(const Iterator& other) const { return current == other.current; }
-        bool operator !=(const Iterator& other) const { return current != other.current; }
-    };
-
-    Iterator begin() { return Iterator(head); }
-    Iterator end() { return Iterator(nullptr); }
-
-    void clear()
-    {
-        head = nullptr;
-        tail = nullptr;
-        size = 0;
-    }
-
-    ~List()
-    {
-        clear();
-    }
-};
-template<typename Iterator, typename T>
-Iterator find(Iterator begin, Iterator end, const T& value)
-{
-    for (auto it = begin; it != end; ++it)
-    {
-        if (*it == value)
-            return it;
-    }
-    return end;
-}
-template<typename Iterator, typename Predicate>
-Iterator find_if(Iterator begin, Iterator end, Predicate pred)
-{
-    for (auto it = begin; it != end; ++it)
-    {
-        if (pred(*it))
-            return it;
-    }
-    return end;
-}
-*/
 bool validate_data(const std::string& title, const std::string& director,
     const int& year, const double& rating, const std::string& genre)
 { 
@@ -375,7 +236,6 @@ private:
                 double rat = std::stod(rating);
 
                 if (validate_data(title, director, year, rat, genre))
-                    //movie_data.push_back(Movie(title, director, year, rat, genre));
                     movies.add(Movie(title, director, year, rat, genre));
             }
             catch (const std::exception except)
@@ -389,7 +249,6 @@ private:
     void save_to_file(const std::string file_name)
     {
         std::ifstream file(file_name);       
-
     }
 public:
     ~FileReader(){}
@@ -403,6 +262,10 @@ public:
     void add_movie(const Movie& movie)
     {
         movies.add(movie);
+    }
+    void delete_movie()
+    {
+
     }
     void search_title(const std::string& prefix) const
     {
@@ -421,7 +284,7 @@ public:
         if (count == 0)
             std::cout << "No matches found!" << std::endl;
     }
-    /*void find_movie() const
+    void find_movie() const
     {
         std::string input;
         char ch;
@@ -443,7 +306,7 @@ public:
                 search_title(input);
             }
         }
-    }*/
+    }
     void print_ascending() const
     {
         movies.print_ascending();
@@ -473,33 +336,44 @@ public:
         {
         case '1':
         {
-            //system("cls");
+            system("cls");
             print_ascending();
             break;
         }
         case '2':
         {
-            //system("cls");
+            system("cls");
             print_descending();
             break;
         }
         case '3':
         {
-            //system("cls");
-            //find_movie();
+            system("cls");
+            find_movie();
             break;
         }
         case '4':
         {
             std::string title, director;
-            std::cout<<"Type movie title : "; std::cin>>title;
+            std::cout<<"Type movie's title : "; std::cin>>title;
             std::cout<<"\nType director : "; std::cin>>director;
-            movies.add(Movie(title, director));
+
+            add_movie(Movie(title, director));
+
             std::cout<<"Successfully added a movie " << title <<" by "<< director <<" to the library!"<<std::endl;
             print_ascending();
-
         }
         case '5':
+        {
+            std::string title;
+            std::cout << "Find movie you want to delete by title : "; std::cin >> title;
+
+            Movie temp_movie(title);
+            movies.delete_(temp_movie);
+            std::cout << "Successfully deleted "<< temp_movie<<std::endl;
+            print_ascending();
+        }
+        case '6':
         {
             end_session();
         }
@@ -509,38 +383,8 @@ public:
     }
 };
 
-class Interface
-{
-private:
-    
-public:
-    BinaryTree<Movie> movies;
-
-    Interface(){}   
-};
-
 int main()
 {
-  /*  List<char> lista_char;
-    std::string zeniucha{ "zeniucha" };
-    
-    auto it = find(lista_char.begin(), lista_char.end(), 'z');
-    if (it!=lista_char.end())
-        std::cout << "Found " << *it << std::endl; 
-   try
-    {
-        FileReader& file_reader = FileReader::getInstance("movies.txt");
-        List<Movie> movies = file_reader.getDataList();
-
-        auto it = find_if(movies.begin(), movies.end(), [](Movie movie) {return movie.getGenre() == "Action"; });
-        if(it != movies.end())
-            std::cout << *it;
-    }
-    catch (const std::exception& except)
-    {
-        std::cout << except.what() << std::endl;
-    }
-*/
     try
     {
         FileReader& file_reader = FileReader::getInstance("movies.txt");
@@ -551,7 +395,6 @@ int main()
     {
         std::cout << except.what() << std::endl;
     }
-
 }
 
 
